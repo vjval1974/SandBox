a0bafa602b2c9270495029c0338e756b616739fc
vjval1974 <w0085400@gmail.com>
2020-10-27 12:50:11 +1000

    Working google solution. Logs in/out and saves

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    
+    'googleAuth' : {
+        'clientID'         : '578660634136-0rvaism87a17m3btucevk7fam66s0jo7.apps.googleusercontent.com',
+        'clientSecret'     : 'b0yz14hhF45hRY-jrsHfeJ_A',
+        'callbackURL'      : 'http://localhost:4000/auth/google/callback'
+    }
+};
